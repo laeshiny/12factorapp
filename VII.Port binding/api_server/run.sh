cd1106c97a8aaba221dfb638b2aa067454a337fd
bonjaymar <bonjaymar@gmail.com>
2016-05-29 16:37:13 +0900

    add run script

---
+++ VII.Port binding/api_server/run.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+
+function build {
+	echo "#######################################################################"
+	echo "### Get package and Build                                           ###"
+	echo "#######################################################################"
+	make get
+	make build
+	echo ""
+}
+
+function start {
+	echo "#######################################################################"
+	echo "### Run api server with port 8001                                   ###"
+	echo "#######################################################################"
+	./bin/api-server 8001 &
+	sleep 1  # For showing process message readably
+	echo ""
+
+	echo "#######################################################################"
+	echo "### Run api server with port 8002                                   ###"
+	echo "#######################################################################"
+	./bin/api-server 8002 &
+	echo ""
+}
+
+case $1 in
+	"run")
+		build
+		start
+		;;
+
+	"build")
+		build
+		;;
+
+	"start")
+		start
+		;;
+
+	"stop")
+		killall api-server
+		;;
+
+esac
